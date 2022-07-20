0915f89e464f5c74e884847250b65ce5b1568a0d
nathanembaye <nathanembaye26@gmail.com>
2022-07-19 21:35:38 -0400

    new links

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function Home() {
             </a>
             {""} and frequent comedy clubs. Feel free to check out my latest project,
             a COVID-19 relief initiative:{" "}
-            <a href="https://github.com/nathanembaye/DekiProject">
+            <a href="https:/dekiottawa.com">
               Deki Ottawa
             </a>
             .
@@ -239,6 +239,12 @@ function Miscellania() {
             <br/>            
             <a href="https://hbr.org/2015/06/the-great-decoupling"><i>The Great Decoupling</i></a>
             <p>Technology is increasing productivity (profit) but reducing the need for some kinds of workers. The pie is being less equally distributed because of it. Brynjolfsson and McAfee explore the implications: who will win, who will lose, and how businesses will need to respond.</p>
+            <a href="https://nypost.com/2019/08/17/luxury-beliefs-are-the-latest-status-symbol-for-rich-americans/"><i>Luxury Beliefs</i></a>
+            <p>Luxury beliefs are ideas and opinions that confer status on the rich at very little cost, while taking a toll on the lower class.</p>
+            <a href="https://bariweiss.substack.com/p/americas-lost-boys-and-me?s=r"><i>America's Lost Boys and Me</i></a>
+            <p>Rob Henderson was in foster-care living out of garbage bags at age seven and smoking weed at nine. Today, a Yale graduate and Ph.D. candidate in Social Psychology at Cambridge, he describes himself as an outsider living in the Ivory Tower.</p>
+            <a href="https://www.theatlantic.com/business/archive/2014/02/how-to-save-marriage-in-america/283732/"><i>How to Save Marriage in America</i></a>
+            <p>The old form of marriage, based on outdated social rules and gender roles, is fading. A new version is emerging—egalitarian, committed, and focused on children.</p>
             <a href="http://www.paulgraham.com/smart.html"><i>Beyond Smart</i></a>
             <p>What was special about Einstein?</p>
             <a href="https://timdillontalks.tumblr.com/post/138638249053/embed"><i>Pizza Hut</i></a>
@@ -249,8 +255,6 @@ function Miscellania() {
             <p>Life in the big city.</p>
             <a href="https://timdillontalks.tumblr.com/post/140455976833/embed"><i>Vive la Crêpe</i></a>
             <p>I’m getting marshmallow though and you’re not.</p>
-            <a href="https://medium.com/poem-of-the-day/charles-bukowski-bluebird-f4e80e5000ef"><i>Bluebird</i></a>
-            <p>Bukowski</p>
             <br/>
             <br/>
           </div>
@@ -259,3 +263,4 @@ function Miscellania() {
 }
 
 export default App;
+
